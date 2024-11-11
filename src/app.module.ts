41812d0b51b66b29c85ef40a967546867e7b70ac
Kate-Shepel <kateborichevskaya@gmail.com>
2024-11-11 17:34:52 +0100

    feat: add favs main functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import { UserModule } from './user/user.module';
 import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
   imports: [
     UserModule,
+    FavoritesModule,
     forwardRef(() => ArtistModule),
     forwardRef(() => TrackModule),
     forwardRef(() => AlbumModule),
